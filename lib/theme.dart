e1d271bbac1f3ea394c336e5e4b33295a93ec278
fiki <fiki@pop-os.localdomain>
2021-08-03 22:23:28 +0700

    add kamusq dir & theme

---
+++ lib/theme.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+
+// theme application
+// Color
+Color blue = Color(0xff004171);
+Color yellow = Color(0xffFFB818);
+Color grey = Color(0xffC4C4C4);
+Color white = Colors.white;
+Color black = Colors.black;
+Color inputColor = Color(0xffF8F8F8);
+Color hintColor = Color(0xffBDBDBD);
+
+// Text Style
+TextStyle blueTextStyle = TextStyle(
+  color: blue,
+);
+
+TextStyle yellowTextStyle = TextStyle(
+  color: yellow,
+);
+
+TextStyle greyTextStyle = TextStyle(
+  color: grey,
+);
+
+TextStyle whiteTextStyle = TextStyle(
+  color: white,
+);
+
+TextStyle blackTextStyle = TextStyle(
+  color: black,
+);
+
+// Font Weight
+FontWeight ligth = FontWeight.w300;
+FontWeight medium = FontWeight.w500;
+FontWeight bold = FontWeight.w700;
+
+
