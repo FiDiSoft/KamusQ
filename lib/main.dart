3d3c06a6ba18b7cd2182262691b9aad576903f1e
ristafn <golemmite23@gmail.com>
2021-09-20 16:28:16 +0700

    initialize Firebase to the main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kamusq/pages/splash_page.dart';
 
-void main() async{
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/': (context)=> SplashPage()
-      },
+      routes: {'/': (context) => SplashPage()},
     );
   }
 }
