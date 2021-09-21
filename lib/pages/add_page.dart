68919bcd2caadbb4a7c331b2fd60d73baf83dc23
ristafn <golemmite23@gmail.com>
2021-09-22 00:04:01 +0700

    add, add page to the project

--- lib/pages/add_page.dart
+++ lib/pages/add_page.dart
@@ -1,19 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:kamusq/models/vocab_services.dart';
 
 class AddPage extends StatefulWidget {
-  const AddPage({ Key? key }) : super(key: key);
+  const AddPage({Key? key}) : super(key: key);
 
   @override
   _AddPageState createState() => _AddPageState();
 }
 
 class _AddPageState extends State<AddPage> {
+  TextEditingController vocabController = TextEditingController(text: '');
+  TextEditingController meaningController = TextEditingController(text: '');
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
         child: Container(
-          
+          child: Column(children: [
+            TextFormField(
+              controller: vocabController,
+            ),
+            TextFormField(
+              controller: meaningController,
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                VocabsServices.addVocab(
+                    vocabController.text.toUpperCase(),
+                    meaningController.text[0].toUpperCase() +
+                        meaningController.text.substring(1));
+
+                vocabController.text = '';
+                meaningController.text = '';
+
+                Navigator.pop(context);
+              },
+              child: Text('add vocab'),
+            )
+          ]),
         ),
       ),
     );
