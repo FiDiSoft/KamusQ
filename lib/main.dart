09f2c41b2477a886c89be38188154f392ca561a1
Fiki Alamsyah <fiki@pop-os.localdomain>
2021-09-24 13:55:13 +0700

    remove some package

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:kamusq/pages/splash_page.dart';
-import 'package:kamusq/theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
